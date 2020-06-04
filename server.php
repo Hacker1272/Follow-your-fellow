45e06fbbfe36434fd6f47bcef3bec0527fca190e
Satya Prakash Sharma <54357789+Hacker1272@users.noreply.github.com>
2020-06-04 16:58:46 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "fdb25.atspace.me";
-$username = "3461248_kalamacademyproject";
-$password = "samvidha@123";
-$database="3461248_kalamacademyproject";
+$servername = "";  //such as- localhost
+$username = "";  //such as- root
+$password = "";  //password for your databse
+$database=""; //name of your database
 $conn = new mysqli($servername, $username, $password,$database);
 if ($conn->connect_error) {
     die("Connection failed: ");
